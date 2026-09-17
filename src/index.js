93748eb2b38b9fd95a17392e9d808262d0242de2
info-vivekranjan <infovivek.ranjan@gmail.com>
2026-09-17 20:45:04 +0530

    DB connection and utils code

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server is listening on PORT - ${PORT}`);
+});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// USING ASYNC/AWAIT
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.status || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
